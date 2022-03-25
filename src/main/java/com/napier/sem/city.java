1daeee91aafb7ab0713ff8f6b6c3439dee119822
moeni12 <40501796@gmail.com>
2022-03-25 10:17:55 +0630

    report all the cities in a country organised by largest population to smallest

---
+++ src/main/java/com/napier/sem/city.java
@@ -0,0 +1,10 @@
+package com.napier.sem;
+
+public class city{
+
+    public int ID;
+    public String Name;
+    public String CountryCode;
+    public String District;
+    public int Population;
+}

353e54d2fb49bf96970fc19209e02403b1e88b55
HninWutyiLae <40501828@live.napier.ac.uk>
2022-04-06 20:09:27 +0630

    testing

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -280,4 +280,67 @@ public class App {
 
     }
 
+    /**
+     * The top N populated cities in a continent where N is provided by the user by HWYL.
+     * @return A list of all city, or null if there is an error.
+     */
+
+    public ArrayList<City> getAllTopcityinContiN(int Num, String ContiN)
+    {
+        try
+        {
+            // Create an SQL statement
+            Statement stmt = con.createStatement();
+            // Create string for SQL statement
+            String strSelect = "SELECT * FROM country, city WHERE country.Code = city.CountryCode and city.Continent = " + "'" + ContiN + "'" + "ORDER BY city.Population desc limit " + Num;
+            // Execute SQL statement
+            ResultSet rset = stmt.executeQuery(strSelect);
+            // Extract employee information
+            ArrayList<City> city = new ArrayList<City>();
+
+
+            while (rset.next())
+            {
+                City emp = new City();
+//                emp.code = rset.getString("country.Code");
+                emp.setCityName(rset.getString("city.Name"));
+                emp.setConame(rset.getString("country.Name"));
+                emp.setDistrict(rset.getString("city.District"));
+                emp.setPopulation (rset.getInt("city.Population"));
+
+
+                city.add(emp);
+
+            }
+            return city;
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+            System.out.println("Failed to get Country details");
+            return null;
+        }
+    }
+
+    public void displayTopCityinContiN(ArrayList<City> world, String filename)
+    {
+        // Check country is not null
+        if (world == null)
+        {
+            System.out.println("No city");
+            return;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        System.out.println("\n-----------------------------------------------------------------------------------\n");
+        System.out.println("The top N populated cities in a country where N is provided by the user. \n ");
+        System.out.println("-----------------------------------------------------------------------------------\n");
+        System.out.println(String.format("%-35s %-40s %-35s %-20s",  "Name", "Country","District","Population"));
+        for (City emp : world)
+        {
+
+            System.out.println(String.format("%-35s %-40s %-35s %-20s",  emp.getCityName(), emp.getCoName(),emp.getDistrict(),emp.getPopulation()));
+        }
+    }
+
 }
